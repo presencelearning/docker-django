fa397954ef37a1cb685daacd7d9bc029d7d623cd
Francois Gaudin <contact@francoisgaudin.com>
2015-03-31 18:15:23 -0700

    Added migrate

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 pip install -r /opt/app/requirements.txt
 
-su django && cd /opt/app && ./manage.py runserver 0.0.0.0:8000
+su django && cd /opt/app && ./manage.py migrate --merge && ./manage.py runserver 0.0.0.0:8000
